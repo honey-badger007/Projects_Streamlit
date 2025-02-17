19c583c048a6defb7bd2849ef2cfeef111b9fdb6
Ahmed Wahba <182025561+honey-badger007@users.noreply.github.com>
2025-02-17 21:24:09 +0200

    Food_waste_app.py

--- Food_waste/Food_waste_app.py
+++ Food_waste/Food_waste_app.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import streamlit as st
 import pickle
 
-st.set_page_config(page_title="Food Waste Prediction", layout="wide")
+#st.set_page_config(page_title="Food Waste Prediction", layout="wide")
 
 st.sidebar.title("Navigation")
 st.sidebar.page_link("Food_waste_app.py", label="🏠 Home", icon="🏡")
@@ -18,7 +18,7 @@ st.title("Welcome to the Food Waste Analysis & Prediction App!")
 st.write(
     """
     📌 Use the **sidebar** to navigate:
-    - 🔮 **Food Waste Prediction**: Predict food waste based on input data.
     - 📊 **Data Visualization**: Explore and analyze the dataset visually.
+    - 🔮 **Food Waste Prediction**: Predict food waste based on input data.
     """
 )
